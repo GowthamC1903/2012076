f3c036cdb693e67e048ba1d99d5e3ff68ad08896
Gowtham C <105162728+GowthamC1903@users.noreply.github.com>
2023-08-11 14:28:36 +0530

    Delete trainRoutes.js

--- Server/trainRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {getTrain,getOneTrain} = require("../trainController")
-
-router.route("/").get(getTrain);
-
-router.route("/:trainNo").get(getOneTrain);
-
-module.exports = router;
